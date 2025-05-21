45c89299e76bf06870830740191a473998fd1377
Sevastian Vascan <sevastian.vascan7@gmail.com>
2025-05-21 18:59:59 +0300

    Initial commit

---
+++ src/test/java/com/example/SB_security/SbSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SB_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
